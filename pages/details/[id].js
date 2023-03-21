1a473f8a540707fb526e6e82e8dcadcc59efd5d8
emma-junior <emnzboy31@gmail.com>
2023-03-21 22:36:24 +0100

    added the title tag

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useGlobalContext } from "../../hooks/DevjobContext";
 import Companysite from "../../components/Companysite";
